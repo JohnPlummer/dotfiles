7b3e2d3a8d752a294d4ace67ee35ca0c54a5b60e
John Plummer <john@memphis.co.uk>
2020-03-04 16:40:16 +0000

    Move aliases to seperate file

---
+++ zsh/aliases.zsh
@@ -0,0 +1,63 @@
+# reload zsh config
+alias reload!='RELOAD=1 source $HOME/.zshrc'
+
+# Use Neovim instead of Vim or Vi
+alias vim="nvim"
+alias vi="nvim"
+
+# Filesystem aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+alias cdh='cd $CODE_DIR/home'
+alias cdgh='cd $CODE_DIR/home/go/src/github.com/johnplummer'
+
+alias l="ls -lah ${colorflag}"
+alias la="ls -AF ${colorflag}"
+alias ll="ls -lFh ${colorflag}"
+alias lld="ls -l | grep ^d"
+alias rmf="rm -rf"
+
+# Helpers
+alias grep='grep --color=auto'
+alias df='df -h' # disk free, in Gigabytes, not bytes
+alias du='du -h -c' # calculate disk usage for a folder
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
+# remove broken symlinks
+alias clsym="find -L . -name . -o -type d -prune -o -type l -exec rm {} +"
+
+# git aliases
+alias gs='git s'
+alias glog="git l"
+alias gho="git open"
+alias grbom="git fetch && git rebase origin/master"
+
+# tmux aliases
+alias ta='tmux attach'
+alias tls='tmux ls'
+alias tat='tmux attach -t'
+alias tns='tmux new-session -s'
+
+# config aliases
+alias zshconfig="vim $HOME/.zshrc"
+alias gitconfig="vim $HOME/.gitconfig"
+alias vimconfig="vim $HOME/.vimrc"
+alias rubocopconfig="vim $HOME/.rubocop.yml"
+
+# ruby
+alias be='bundle exec'
+alias ber='bundle exec rake'
+alias tber='RAILS_ENV=test bundle exec rake'
+
+# work aliases
+alias cdgw='cd $CODE_DIR/deliveroo/go/src/github.com/deliveroo'
+alias cdw='cd $CODE_DIR/deliveroo'
+alias shs-production="ssh ecs@hopper-production self-help-service web hopper-runner bundle exec rails c"
+alias shs-staging="ssh ecs@hopper-staging self-help-service web hopper-runner bundle exec rails c"
+alias ssh-shs-staging="ssh ecs@hopper-staging self-help-service web hopper-runner sh"
+alias ssh-atlas-staging="ssh ecs@hopper-staging logistics-dashboard web hopper-runner sh"
+
